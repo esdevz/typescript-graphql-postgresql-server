c09c51bf5555bf5db77ab7b2d209369db2c883c3
Wael-Hx <wael.lhb@gmail.com>
2021-01-22 18:44:10 +0100

    Users register module

---
+++ src/db/modules/Users.ts
@@ -0,0 +1,21 @@
+import { User } from "src/graphql/typeDefs/users";
+import pool from "../client";
+
+export default {
+  async register(
+    username: string,
+    email: string,
+    password: string
+  ): Promise<User | string> {
+    try {
+      const user = await pool.query(
+        "INSERT INTO users(username , email , password) VALUES($1,$2,$3) RETURNING *",
+        [username, email, password]
+      );
+      return user.rows[0];
+    } catch (err) {
+      console.error(err);
+      return err.message;
+    }
+  },
+};
