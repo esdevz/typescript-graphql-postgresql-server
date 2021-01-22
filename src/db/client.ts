c37b8d31cde416d04a3c3ac48d06ae28894a482a
Wael-Hx <wael.lhb@gmail.com>
2021-01-22 18:42:16 +0100

    use pool instead of client

---
+++ src/db/client.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: 5432,
+});
+
+export default pool;
