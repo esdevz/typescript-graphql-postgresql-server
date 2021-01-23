63683c28643692b4bb502eca9b0cb21b0e05f8ed
Wael-Hx <wael.lhb@gmail.com>
2021-01-23 15:02:00 +0100

    refactor register + login Resolver

--- src/graphql/resolvers/users/user.ts
+++ src/graphql/resolvers/users/user.ts
@@ -1,6 +1,8 @@
 import { Arg, Mutation, Resolver } from "type-graphql";
-import { Credentials, User } from "./user.type";
+import { Credentials, LoginCredentials, User } from "./user.type";
 import Users from "../../../db/modules/Users";
+import { UserInputError } from "apollo-server";
+import argon2 from "argon2";
 
 @Resolver()
 export class UserResolver {
@@ -8,7 +10,31 @@ export class UserResolver {
   async register(
     @Arg("credentials") { username, email, password }: Credentials
   ) {
-    const user = await Users.register(username, email, password);
+    const exists = await Users.getUser("email", email);
+    if (exists) {
+      return new UserInputError("user already exists");
+    }
+    const hashedPass = await argon2.hash(password);
+    const user = await Users.register(username, email, hashedPass);
     return user;
+  }
+
+  @Mutation(() => User)
+  async login(@Arg("credentials") { email, password }: LoginCredentials) {
+    try {
+      const user = await Users.getUser("email", email);
+      if (!user) {
+        return new UserInputError("invalid credentials");
+      }
+
+      const validPass = await argon2.verify(user.password, password);
+      if (!validPass) {
+        return new UserInputError("invalid credentials");
+      }
+      return user;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
   }
 }
