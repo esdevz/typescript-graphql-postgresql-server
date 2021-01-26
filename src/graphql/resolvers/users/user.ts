0333115bf1b0f6d79889851271aa30aaf7bf6b17
Wael-Hx <wael.lhb@gmail.com>
2021-01-26 12:19:18 +0100

    current user query

--- src/graphql/resolvers/users/user.ts
+++ src/graphql/resolvers/users/user.ts
@@ -1,26 +1,51 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { Credentials, LoginCredentials, User } from "./user.type";
 import Users from "../../../db/modules/Users";
-import { UserInputError } from "apollo-server";
+import { AuthenticationError, UserInputError } from "apollo-server";
 import argon2 from "argon2";
+import { MyCtx } from "src/graphql/types";
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
-  async register(
-    @Arg("credentials") { username, email, password }: Credentials
-  ) {
-    const exists = await Users.getUser("email", email);
-    if (exists) {
-      return new UserInputError("user already exists");
+  @Query(() => User)
+  async me(@Ctx() { userId }: MyCtx) {
+    try {
+      if (!userId) {
+        return new AuthenticationError("not authorized");
+      }
+      const user = await Users.getUser("id", userId);
+      return user;
+    } catch (err) {
+      console.log(err);
+      return err;
     }
-    const hashedPass = await argon2.hash(password);
-    const user = await Users.register(username, email, hashedPass);
-    return user;
   }
 
   @Mutation(() => User)
-  async login(@Arg("credentials") { email, password }: LoginCredentials) {
+  async register(
+    @Arg("credentials") { username, email, password }: Credentials,
+    @Ctx() { req }: MyCtx
+  ) {
+    try {
+      const exists = await Users.getUser("email", email);
+      if (exists) {
+        return new UserInputError("user already exists");
+      }
+      const hashedPass = await argon2.hash(password);
+      const user = await Users.register(username, email, hashedPass);
+      req.session.userId = user.id;
+      return user;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+
+  @Mutation(() => User)
+  async login(
+    @Arg("credentials") { email, password }: LoginCredentials,
+    @Ctx() { req }: MyCtx
+  ) {
     try {
       const user = await Users.getUser("email", email);
       if (!user) {
@@ -31,6 +56,7 @@ export class UserResolver {
       if (!validPass) {
         return new UserInputError("invalid credentials");
       }
+      req.session.userId = user.id;
       return user;
     } catch (err) {
       console.error(err);
