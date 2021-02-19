811d60890ebe0885378f9f88c8d6dc67372d3145
Wael-Hx <wael.lhb@gmail.com>
2021-02-19 18:52:07 +0100

    new group input type

--- src/graphql/resolvers/communities/communities.type.ts
+++ src/graphql/resolvers/communities/communities.type.ts
@@ -1,4 +1,5 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { IsNotEmpty } from "class-validator";
+import { Field, ID, InputType, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class Community {
@@ -16,4 +17,20 @@ export class Community {
 
   @Field(() => ID)
   comm_admin: number;
+}
+
+@InputType()
+export class GroupDetails {
+  @Field()
+  @IsNotEmpty({ message: "group name is required" })
+  name: string;
+
+  @Field({ nullable: true })
+  cover?: string;
+
+  @Field({ nullable: true })
+  cover_image?: string;
+
+  @Field()
+  description: string;
 }
