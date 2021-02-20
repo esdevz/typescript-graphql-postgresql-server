4935f2f0629cf41dbc58b044c08b806534c14e00
Wael-Hx <wael.lhb@gmail.com>
2021-02-20 16:00:40 +0100

    contacts / members methods

--- src/db/modules/Users.ts
+++ src/db/modules/Users.ts
@@ -1,6 +1,8 @@
 import { Community } from "src/graphql/resolvers/communities/communities.type";
+import { Contact } from "../../graphql/resolvers/users/contacts.type";
 import { User } from "src/graphql/resolvers/users/user.type";
 import pool from "../client";
+import { formatQuery } from "../../utils/index";
 
 export default {
   async register(
@@ -104,6 +106,71 @@ export default {
       return err;
     } finally {
       client.release();
+    }
+  },
+  async getMyContacts(userId: number): Promise<Contact[]> {
+    try {
+      const contacts = await pool.query(
+        `select id , username ,avatar from contact_list
+       inner join users on users.id = contact_list.contact where
+       primary_user =$1 `,
+        [userId]
+      );
+      return contacts.rows;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+  },
+  async addContact(userId: number, contactId: number): Promise<Contact> {
+    const client = await pool.connect();
+    try {
+      await client.query("BEGIN");
+      const contact = await client.query(
+        `with new_contact as (
+          insert into contact_list(primary_user,contact)
+           values($1 , $2)
+          returning contact as contact_id
+          )
+          select  contact_id as id , username , avatar from new_contact inner join users on users.id = contact_id`,
+        [userId, contactId]
+      );
+
+      await client.query("COMMIT");
+      return contact.rows[0];
+    } catch (err) {
+      await client.query("ROLLBACK");
+      console.error(err);
+      return err;
+    } finally {
+      client.release();
+    }
+  },
+  async getContactList(): Promise<Contact[]> {
+    try {
+      const contactList = await pool.query(
+        `select id , username , avatar from users`
+      );
+      return contactList.rows;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+  },
+  async addMembers(users: number[]): Promise<string> {
+    const client = await pool.connect();
+    try {
+      let queryString = formatQuery(users);
+      await client.query("BEGIN");
+      await client.query(
+        `INSERT INTO members(community_id , member_id) values ${queryString}`,
+        users
+      );
+      await client.query("COMMIT");
+      return "members added";
+    } catch (err) {
+      console.error(err);
+      return "an error accured while adding members";
     }
   },
 };
