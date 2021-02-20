a35c405fc3f4b031705970bac1bc43d9d77bc258
Wael-Hx <wael.lhb@gmail.com>
2021-02-20 16:00:15 +0100

    formatQuery

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,3 +12,8 @@ export const getSession = (session: MySessionData): number | null => {
 export const containsStr = (source: string, target: string[]): boolean => {
   return target.every((str) => source.includes(str));
 };
+
+export const formatQuery = (list: number[]): string => {
+  const values = list.map((_, idx) => `($1 , $${idx + 1})`);
+  return values.slice(1).join(",");
+};
