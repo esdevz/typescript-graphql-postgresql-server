b254be053cc36fe5cdae897fe1084876d03ae120
Wael-Hx <wael.lhb@gmail.com>
2021-03-10 14:05:42 +0100

    add / get messages resolver

--- src/graphql/subscriptions/chat/chat.ts
+++ src/graphql/subscriptions/chat/chat.ts
@@ -1,14 +1,18 @@
 import {
   Arg,
+  Ctx,
   ID,
   Mutation,
   PubSub,
   PubSubEngine,
+  Query,
   Resolver,
   Root,
   Subscription,
 } from "type-graphql";
 import { ChatMessage } from "./chat.type";
+import Messages from "../../../db/modules/Messages";
+import { MyCtx } from "src/graphql/types";
 
 @Resolver()
 export class Chat {
@@ -21,17 +25,45 @@ export class Chat {
     @Arg("id", () => ID) id: string
   ): Promise<string> {
     try {
+      let timestamp = Date.now();
+      await Messages.addMessage(
+        parseInt(to),
+        parseInt(id),
+        username,
+        body,
+        timestamp
+      );
       await pubsub.publish(to, {
         sub: to,
         username,
         id,
         body: body || "...",
-        timestamp: Date.now(),
+        timestamp,
       });
       return "message sent";
     } catch (err) {
       console.error(err);
       return "cannot send message , try again later";
+    }
+  }
+
+  @Query(() => [ChatMessage])
+  async getMessages(
+    @Ctx() { userId }: MyCtx,
+    @Arg("subs", () => [ID]) subs: string[]
+  ) {
+    if (!userId) {
+      return [];
+    }
+    try {
+      const messages = await Messages.getMessages(
+        subs.map((sub) => parseInt(sub))
+      );
+
+      return messages;
+    } catch (err) {
+      console.error(err);
+      return err;
     }
   }
 
