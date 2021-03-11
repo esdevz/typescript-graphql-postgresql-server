84bb989f10b796f784068fa7e82a18254545a26d
Wael-Hx <wael.lhb@gmail.com>
2021-03-11 19:31:16 +0100

    return messages count & delete old messages

--- src/db/modules/Messages.ts
+++ src/db/modules/Messages.ts
@@ -9,20 +9,21 @@ export default {
     username: string,
     body: string,
     timestamp: number
-  ): Promise<string> {
+  ): Promise<number> {
     const client = await pool.connect();
     try {
       await client.query("BEGIN");
-      await client.query(
-        `insert into messages(sub ,id ,username , body , timestamp ) values($1,$2,$3,$4,$5)`,
+      const count = await client.query(
+        `with new_msg as (insert into messages(sub ,id ,username , body , timestamp ) values($1,$2,$3,$4,$5) returning sub )
+        select count(msg_id) from messages where sub = (select sub from new_msg) `,
         [sub, id, username, body, timestamp]
       );
       await client.query("COMMIT");
-      return "message saved";
+      return parseInt(count.rows[0].count);
     } catch (err) {
       await client.query("ROLLBACK");
       console.error(err);
-      return "cannot save message";
+      return 0;
     } finally {
       client.release();
     }
@@ -32,7 +33,7 @@ export default {
       let queryString = formatMsgQuery("sub", subs);
       const messages = await pool.query(
         `select sub ,id ,username , body , timestamp from messages
-             where (${queryString})`,
+             where (${queryString}) order by timestamp asc `,
         subs
       );
 
@@ -40,6 +41,24 @@ export default {
     } catch (err) {
       console.error(err);
       return [];
+    }
+  },
+  async deleteMessages(sub: number, count: number): Promise<void> {
+    const client = await pool.connect();
+    try {
+      let messagesLimit = count - 50;
+      await client.query("BEGIN");
+      await client.query(
+        `delete from messages where msg_id in
+       ( select msg_id from messages where sub = $1 order by msg_id asc limit $2 )`,
+        [sub, messagesLimit]
+      );
+      await client.query("COMMIT");
+    } catch (err) {
+      await client.query("ROLLBACK");
+      console.error(err);
+    } finally {
+      client.release();
     }
   },
 };
